d2f40ab59ff60bddbc5edfa4345961fc0d3579d8
sam <samuelkinyua722@gmail.com>
2023-08-17 15:04:01 +0300

    added the humberger menu for mobile mode and x for canceling

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const navigation = document.getElementsByClassName("nav");
+const mobileNav = document.querySelector(".nav-bar");
+const navToggle = document.querySelector(".hamburger-menu");
 
-navigation.addEventListerner('click', e =>{
-    if(this.classList.contains("active")){
-        this.classList.remove("active");
-    }else this.classList.add("active");
+navToggle.addEventListener("click", () => {
 
+    const visibility = mobileNav.getAttribute("data-visible");
+   
+    if (visibility === "false") {
+        mobileNav.setAttribute("data-visible", "true");
+        navToggle.setAttribute("aria-expanded","true");
+    } else if (visibility === "true") {
+        mobileNav.setAttribute("data-visible", "false");
+        navToggle.setAttribute("aria-expanded", "false");
+    }
 });
+
